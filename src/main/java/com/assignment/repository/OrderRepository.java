79289a231c23b7d9a2ee2f99d743fb1c2555b0f9
ShubhaDevarajan <33474208+ShubhaDevarajan@users.noreply.github.com>
2023-02-21 10:47:43 +0100

    Update OrderRepository.java

--- src/main/java/com/assignment/repository/OrderRepository.java
+++ src/main/java/com/assignment/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package com.assignment.repository;
 
 import com.assignment.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Orders, String> {
 
     List<Orders> findByEmail(String email);
